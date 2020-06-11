0098b081c12bbcc8f8c10da3876f7dad4de162e4
jiang <80260990@qq.com>
2020-06-11 19:11:14 +0800

    从idea提交2

---
+++ git-test-project/src/main/java/com/itheima/git/FirstClass.java
@@ -0,0 +1,25 @@
+package com.itheima.git;
+
+/**
+ * @Classname FirstClass
+ * @Description TODO
+ * @Date 2020/6/11 18:58
+ * @Created by Leslie
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+
+
+    public void test(){
+        int i = 0;
+        i+=10;
+    }
+}
